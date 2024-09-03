17b7a0d8a65d882d87d41e40e39e3d54844da78f
vikki <vikki@right2vote.in>
2024-09-03 23:54:40 +0530

    databse connection

---
+++ streamium backend/src/db/index.js
@@ -0,0 +1,14 @@
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`);
+        console.log(`MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Database Connection Error ", error);
+        process.exit(1);
+    }
+};
+export default connectDB;
